34380027fbdc3e5adbeb47f11dbb783b0182c1de
naaa <na0908nna@gmail.com>
2025-03-15 18:11:18 +0300

    integrating custom user  model commit

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm
-from .models import Room
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Room,User
 
 
 class RoomForm(ModelForm):
@@ -13,4 +13,4 @@ class RoomForm(ModelForm):
 class UserForm(ModelForm):
      class Meta:
         model=User
-        fields=['username','email']
+        fields=['avatar','name','username','email','bio']
--- carechat/settings.py
+++ carechat/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
 
 ]
 
+AUTH_USER_MODEL = 'base.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     
@@ -126,13 +128,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 
 
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
